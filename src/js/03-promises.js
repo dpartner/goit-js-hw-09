20ebc12a3a584f3490c7c3a4385ef5a83ece889f
dpartner <d.partner.ua@gmail.com>
2022-04-04 13:54:52 +0300

    3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const ref = {
+  form: document.querySelector('.form'),
+};
+
+let promiseCount = 0;
+
+ref.form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const delay = Number(e.currentTarget.delay.value);
+  const delayStep = Number(e.currentTarget.step.value);
+  const amount = Number(e.currentTarget.amount.value);
+  let newDelay = delay;
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, newDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    newDelay += delayStep;
   }
 }
